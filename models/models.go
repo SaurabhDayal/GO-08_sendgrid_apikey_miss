ffe087ffb96df3d4c90861bc3e77f5b8b9d68e21
Saurabh <109336554+SaurabhDayal@users.noreply.github.com>
2024-06-20 00:16:36 +0530

    sendGrid key not available due to vetting process, learned the sendgrid account console and template working

--- models/models.go
+++ models/models.go
@@ -4,10 +4,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID          primitive.ObjectID `json:"-" bson:"_id,omitempty"`
-	Email       string             `json:"email"`
-	FirstName   string             `json:"firstName"`
-	LastName    string             `json:"lastName"`
-	Number      string             `json:"number"`
-	CountryCode string             `json:"countryCode"`
+	Email       string             `json:"email" json:"email"`
+	FirstName   string             `json:"firstName" bson:"firstName"`
+	LastName    string             `json:"lastName" bson:"lastName"`
+	Number      string             `json:"number" bson:"number"`
+	CountryCode string             `json:"countryCode" bson:"countryCode"`
 	Occupation  string             `json:"occupation" bson:"occupation"`
 }
